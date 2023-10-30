2ffa3376b696a292c39b8d4b86a18703f9d4646a
bishalprasad321 <bishalprasad321@gmail.com>
2023-10-30 15:48:04 +0530

    Working with deletion of account of logged in user

--- app/src/main/java/com/bishal/incubator/SettingsActivity.kt
+++ app/src/main/java/com/bishal/incubator/SettingsActivity.kt
@@ -1,55 +1,111 @@
 package com.bishal.incubator
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.bishal.incubator.databinding.ActivitySettingsBinding
+import com.bishal.incubator.utils.USER_NODE
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class SettingsActivity : AppCompatActivity() {
 
-    private val binding : ActivitySettingsBinding by lazy {
+    private val binding: ActivitySettingsBinding by lazy {
         ActivitySettingsBinding.inflate(layoutInflater)
     }
+
+    // Google sign in options
+    private lateinit var gso: GoogleSignInOptions
+
+    // Google sign in client
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    private lateinit var user: FirebaseUser
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        user = FirebaseAuth.getInstance().currentUser!!
+        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this@SettingsActivity, gso)
 
         // Back button
         binding.backButton.setOnClickListener {
             finish()
         }
 
-        // Google sign in options
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
-            .requestEmail()
-            .build()
-
-        // Google sign in client
-        val googleSignInClient: GoogleSignInClient = GoogleSignIn.getClient(this@SettingsActivity, gso)
-
         binding.signOutButton.setOnClickListener {
-            googleSignInClient.signOut()
-                .addOnCompleteListener(this@SettingsActivity) {
-                    if (it.isSuccessful) {
-                        startActivity(Intent(this@SettingsActivity, SignInActivity::class.java))
-                        finish()
-                    } else {
-                        Toast.makeText(
-                            this@SettingsActivity,
-                            it.exception?.localizedMessage,
-                            Toast.LENGTH_LONG
-                        ).show()
+            signOut()
+        }
+
+        // Delete account button
+        binding.deleteButton.setOnClickListener {
+
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this@SettingsActivity)
+            builder
+                .setMessage("Once deleted, account cannot be recovered!")
+                .setTitle("Permanently delete your account?")
+                .setPositiveButton("Delete") { dialog, _ ->
+                    dialog.dismiss()
+                    binding.settingsProgressBar.visibility = View.VISIBLE
+                    user.delete().addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            googleSignInClient.signOut()
+                            Firebase.auth.signOut()
+                            binding.settingsProgressBar.visibility = View.GONE
+                            Firebase.firestore.collection(USER_NODE).document(user.uid).delete()
+                            Toast.makeText(
+                                this@SettingsActivity,
+                                "Account deleted successfully",
+                                Toast.LENGTH_LONG
+                            ).show()
+                            startActivity(Intent(this@SettingsActivity, SignInActivity::class.java))
+                            finish()
+                        } else {
+                            binding.settingsProgressBar.visibility = View.INVISIBLE
+                            Toast.makeText(
+                                this@SettingsActivity,
+                                it.exception?.localizedMessage,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
                     }
                 }
-            Firebase.auth.signOut()
+                .setNegativeButton("No"){ dialog, _ ->
+                    dialog.dismiss()
+                }
+
+            builder.create().show()
         }
+    }
+
+    private fun signOut() {
+        googleSignInClient.signOut()
+            .addOnCompleteListener(this@SettingsActivity) {
+                if (it.isSuccessful) {
+                    startActivity(Intent(this@SettingsActivity, SignInActivity::class.java))
+                    finish()
+                } else {
+                    Toast.makeText(
+                        this@SettingsActivity,
+                        it.exception?.localizedMessage,
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        Firebase.auth.signOut()
     }
 }
